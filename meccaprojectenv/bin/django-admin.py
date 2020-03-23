918e9805131a43a10a4075cc8254b126cfa84f18
WSINTRA <will.simpson85@gmail.com>
2020-03-22 20:20:11 -0400

    django db connected in settings

---
+++ meccaprojectenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Music_Studio/Development/BingoApp/bingo-server/meccaprojectenv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
